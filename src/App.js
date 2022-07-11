8f45b3e371b4243f1d8f85fb8bccca569f3c9f74
Dhaval Kumar <dhavalkumar741@gmail.com>
2022-07-11 15:30:02 +0530

    added alt header and navbar

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
 // import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
-import Footer from"./components/Footer";
-import Statistics from"./components/Statistics";
-import back from './resources/bg1.jpg';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Statistics from "./components/Statistics";
+import back from "./resources/bg1.jpg";
+import Altnavbar from "./components/Altnavbar";
 
-import Aboutus from './components/AboutUs';
+import Aboutus from "./components/AboutUs";
 
-import Timeline from './components/Timeline';
-import TestimonyDirector from './components/TestimonyDirector';
-import Recruitus from './components/Recruitus';
-
+import Timeline from "./components/Timeline";
+import TestimonyDirector from "./components/TestimonyDirector";
+import Recruitus from "./components/Recruitus";
+import Altheader from "./components/Altheader";
 
 function App() {
   return (
     <>
-    <Navbar />
-    <br/>
-    <Aboutus/>
-    <br/>
+      {/* <Navbar /> */}
+      <Altnavbar />
+      <Altheader />
+      <br />
+      <Aboutus />
+      <br />
 
-    <br/>
-    <TestimonyDirector/>
-    <br />
+      <br />
+      <TestimonyDirector />
+      <br />
 
-    <Recruitus />
-    <br />
+      <Recruitus />
+      <br />
 
-    <Timeline />
-    <br />
-    <Statistics />
-    <br />
-    <Footer />
+      <Timeline />
+      <br />
+      <Statistics />
+      <br />
+      <Footer />
     </>
   );
 }
