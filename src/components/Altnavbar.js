74e4f33baccd7622da1ae3fdcd2584a5386d1558
Dhaval Kumar <dhavalkumar741@gmail.com>
2022-07-11 17:28:31 +0530

    fixed navbar size

--- src/components/Altnavbar.js
+++ src/components/Altnavbar.js
@@ -40,10 +40,10 @@ const Altnavbar = () => {
   ));
 
   return (
-    <nav class="bg-blue border-gray-200 px-2 sm:px-4 dark:bg-gray-800" >
+    <nav class="bg-blue border-gray-200 py-3 sm:py-2 px-2 sm:px-4 dark:bg-gray-800" >
       <div class="container flex flex-wrap justify-between items-center mx-auto">
         <a href="https://www.iiitnr.ac.in/" class="flex items-center">
-          <img src={logo} class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+          <img src={logo} class="mr-3 h-6 sm:h-9" alt="IIIT Naya Raipur Logo" />
           <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
             Placement Cell IIIT-Naya Raipur
           </span>
