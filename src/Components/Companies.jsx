5067b0fb7272a657bec9bc8dff19a96a9295da37
Dhaval Kumar <dhavalkumar741@gmail.com>
2022-07-16 00:22:34 +0530

    fixed companies section for mobile view

--- src/Components/Companies.jsx
+++ src/Components/Companies.jsx
@@ -9,10 +9,10 @@ AOS.init();
 const Companies = () => {
   const listImages = images.map((element) => (
     <a href={element.website} class="border border-white rounded-xl drop-shadow-2xl shadow-lg bg-white hover:scale-110 transition-all duration-300 cursor-pointer flex justify-center items-center ">
-        <img class="mx-auto w-[100px] aspect-auto" src={element.image} key={element.id} alt={`${element.name} logo`} loading="lazy" />
+        <img class="mx-auto h-[100px] w-fit md:w-[100px] md:h-fit aspect-auto" src={element.image} key={element.id} alt={`${element.name} logo`} loading="lazy" />
     </a>
   ));
-  const elementInRow = 4;
+  const elementInRow = 6;
   const noImages = images.length;
   return (
     <div data-aos="fade-down">
@@ -30,7 +30,7 @@ const Companies = () => {
       </h1>
       <br />
       <div class="py-3 px-10 place-items-center">
-        <div class={`grid grid-flow-row grid-cols-6 grid-rows-1 md:grid-cols-${elementInRow} md:grid-row-${Math.ceil(noImages / elementInRow)} gap-8`}>
+        <div class={`grid grid-flow-row grid-cols-1 md:grid-cols-${elementInRow} gap-8`}>
           {listImages}
         </div>
       </div>
