aebac52a4055b72217e82708eb84aca6a8041f99
adityagoel28 <adityagoel2002@gmail.com>
2022-07-15 23:21:39 +0530

    companies added

--- src/images.js
+++ src/images.js
@@ -5,6 +5,26 @@ import image4 from "./assets/companies/hyperverge.png";
 import image5 from "./assets/companies/juspay.webp";
 import image6 from "./assets/companies/pharmeasy.png";
 
+import image19 from "./assets/companies/Credgenics.jpg";
+import image20 from "./assets/companies/deltax.png";
+import image21 from "./assets/companies/digite.png";
+import image22 from "./assets/companies/lumiq.png";
+import image23 from "./assets/companies/HashedIn.jpg";
+import image24 from "./assets/companies/govivace.png";
+import image25 from "./assets/companies/lti.png";
+import image26 from "./assets/companies/infosys.png";
+import image27 from "./assets/companies/jtg.png";
+import image28 from "./assets/companies/tricon-infotech.png";
+import image29 from "./assets/companies/ericsson.png";
+import image30 from "./assets/companies/wd.png";
+import image31 from "./assets/companies/zycus.png";
+import image32 from "./assets/companies/teradata.png";
+import image33 from "./assets/companies/tekion.jpg";
+import image34 from "./assets/companies/vassarLabs.jpg";
+import image35 from "./assets/companies/steradian.jpeg";
+import image36 from "./assets/companies/tiger-analytics.png";
+
+
 export default [
   {
     id: 1,
@@ -41,5 +61,113 @@ export default [
     name: "PharmEasy",
     website: "https://pharmeasy.in/",
     image: image6
-  }
+  },
+  {
+    id: 19,
+    name: "Credgenics",
+    website: "https://www.credgenics.com/",
+    image: image19,
+  },
+  {
+    id: 20,
+    name: "DeltaX",
+    website: "https://www.deltax.com/",
+    image: image20,
+  },
+  {
+    id: 21,
+    name: "Digite",
+    website: "https://www.digite.com/",
+    image: image21,
+  },
+  {
+    id: 22,
+    name: "Lumiq",
+    website: "https://lumiq.ai/",
+    image: image22,
+  },
+  {
+    id: 23,
+    name: "HashdeIn",
+    website: "https://hashedin.com/",
+    image: image23,
+  },
+  {
+    id: 24,
+    name: "GoVivace",
+    website: "https://www.govivace.com/",
+    image: image24,
+  },
+  {
+    id: 25,
+    name: "LTI",
+    website: "https://www.lntinfotech.com/",
+    image: image25,
+  },
+  {
+    id: 26,
+    name: "Infosys",
+    website: "https://www.infosys.com/",
+    image: image26,
+  },
+  {
+    id: 27,
+    name: "JTG",
+    website: "https://www.joshtechnologygroup.com/",
+    image: image27,
+  },
+  {
+    id: 28,
+    name: "Tricon Infotech",
+    website: "https://www.triconinfotech.com/",
+    image: image28,
+  },
+  {
+    id: 29,
+    name: "Ericsson",
+    website: "https://www.ericsson.com/en",
+    image: image29,
+  },
+  {
+    id: 30,
+    name: "Western Digital",
+    website: "https://www.americanexpress.com/en-in/",
+    image: image30,
+  },
+  {
+    id: 31,
+    name: "Zycus",
+    website: "https://www.zycus.com/",
+    image: image31,
+  },
+  {
+    id: 32,
+    name: "Teradata",
+    website: "https://www.teradata.com/",
+    image: image32,
+  },
+  {
+    id: 33,
+    name: "Tekion",
+    website: "https://tekion.com/",
+    image: image33,
+  },
+  {
+    id: 34,
+    name: "Vassar Labs",
+    website: "https://www.vassarlabs.com/",
+    image: image34,
+  },
+  {
+    id: 35,
+    name: "Steradian",
+    website: "https://steradiansemi.com/",
+    image: image35,
+  },
+  {
+    id: 36,
+    name: "Tiger Analytics",
+    website: "https://www.tigeranalytics.com/",
+    image: image36,
+  },
 ];
